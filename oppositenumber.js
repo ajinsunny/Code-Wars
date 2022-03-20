2d4d094bc766ee18e1b4320cdbaffb1652fd14a5
Brynn Smith <99383283+brynnsmith@users.noreply.github.com>
2022-03-20 16:41:55 -0400

    Delete oppositenumber.js

--- oppositenumber.js
+++
@@ -1,9 +0,0 @@
-// Very simple, given an integer or a floating-point number, find its opposite.
-
-function opposite(number) {
-  if (number == -number) { 
-    return Math.sign(number)
-} if (number == number) {
-  return -Math.sign(number);
-}
-}
