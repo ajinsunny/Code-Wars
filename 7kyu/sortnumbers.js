b662e4510d8476daf501be482528ea3286a60f11
Brynn Smith <99383283+brynnsmith@users.noreply.github.com>
2022-03-23 05:04:04 -0400

    Create sortnumbers.js

---
+++ 7kyu/sortnumbers.js
@@ -0,0 +1,3 @@
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+const solution = nums => nums === null? [] : nums.sort((a, b) => a - b)
