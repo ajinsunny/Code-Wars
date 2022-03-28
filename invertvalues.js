594692744f486b79c1e17765c3c77e8b14eade67
Brynn Smith <99383283+brynnsmith@users.noreply.github.com>
2022-03-28 05:20:11 -0400

    Create invertvalues.js

---
+++ invertvalues.js
@@ -0,0 +1,3 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+const invert = array => array.map((a, b) => a * -1)
