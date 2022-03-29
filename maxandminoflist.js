38061a0b2e234a71a964073a090c20b0f58e3c26
Brynn Smith <99383283+brynnsmith@users.noreply.github.com>
2022-03-29 18:48:45 -0400

    Create maxandminoflist.js

---
+++ maxandminoflist.js
@@ -0,0 +1,4 @@
+/* Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list. */
+
+const min = list => Math.min(...list)
+const max = list => Math.max(...list)
