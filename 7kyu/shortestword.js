e8628a216956b8e576bd9981b48a17edd523e664
Brynn Smith <brynn@genesisbox.ca>
2022-04-05 19:04:29 -0400

    Create shortestword.js

---
+++ 7kyu/shortestword.js
@@ -0,0 +1,8 @@
+/*Simple, given a string of words, return the length of the shortest word(s).
+ String will never be empty and you do not need to account for different data types. */
+
+ function findShort(s){
+    arr = s.split(" ")
+    arr.sort((a, b) => a.length - b.length)
+    return arr[0].length;
+  }
