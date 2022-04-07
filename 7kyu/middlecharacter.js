60b6a54da131de16b71ca7131476f3f9ced0ddf3
Brynn Smith <brynn@genesisbox.ca>
2022-04-06 22:55:49 -0400

    Create middlecharacter.js

--- 7kyu/middlecharacter.js
+++ 7kyu/middlecharacter.js
@@ -4,6 +4,6 @@ function getMiddle(s){
     if (s.length % 2 !== 0) {
       return (s[Math.floor(s.length/2)])
     } else if (s.length % 2 === 0) {
-      return s[Math.floor(s.length/2 - 1)] + s[Math.floor(s.length/2 - 1) + 1]
+      return s[s.length/2 - 1] + s[s.length/2]
     }
   }
