aaa2564ad884c42b0c2df6e65cc0899483da9333
Brynn Smith <brynn@genesisbox.ca>
2022-04-10 02:59:37 -0400

    Create creditcardmask.js

---
+++ 7kyu/credidcardmask.js
@@ -0,0 +1,11 @@
+/* Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'. */
+
+const maskify = cc => {
+    if (cc.length <= 4) {
+      return cc
+    } else { 
+      return "#".repeat(cc.length-4) + cc.substr(-4)
+     }
+    }
