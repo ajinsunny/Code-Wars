d4d3e75af2c24d4aded0409bf01cf5dc9b0be6a7
Brynn Smith <brynn@genesisbox.ca>
2022-05-21 23:33:08 -0400

    Create removeexclamationmarks.js

---
+++ 7kyu/factorial2.js
@@ -0,0 +1,17 @@
+/* In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+
+More details about factorial can be found here. */
+
+function factorial(n){
+    let product = 1
+     if (n >= 0 && n <= 12) {
+       for (let i = n; i > 0; i--){
+         product *= i
+       }
+       return product
+     }
+    throw RangeError
+    return -1
+  }
